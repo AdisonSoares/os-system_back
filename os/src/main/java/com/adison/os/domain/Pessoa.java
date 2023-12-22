d1d55a0c6f8fdf20da29ace08ee36b6b136c59dd
adison <adison.soares7@gmail.com>
2023-12-21 22:59:34 -0300

    :sparkles: Feat: configure the domain with the bank

--- os/src/main/java/com/adison/os/domain/Pessoa.java
+++ os/src/main/java/com/adison/os/domain/Pessoa.java
@@ -1,13 +1,25 @@
 package com.adison.os.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+@Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 public abstract class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nome;
+
+    @CPF
     private String cpf;
+
     private String telefone;
 }
