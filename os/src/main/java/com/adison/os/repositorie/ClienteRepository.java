1b88a8752957689c110046d7e3fdc6bb07298c4d
adison <adison.soares7@gmail.com>
2023-12-22 19:28:27 -0300

    :sparkles: Feat: add the domain repository

---
+++ os/src/main/java/com/adison/os/repositorie/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.adison.os.repositorie;
+
+import com.adison.os.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+}
