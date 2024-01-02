c8893088976ba0ec3200803244ef26892d7d2fcb
adison <adison.soares7@gmail.com>
2024-01-02 12:29:09 -0300

    :sparkles: Feat: add the technical controller

--- os/src/main/java/com/adison/os/controller/TecnicoController.java
+++ os/src/main/java/com/adison/os/controller/TecnicoController.java
@@ -1,2 +1,23 @@
-package com.adison.os.controller;public class TecnicoController {
+package com.adison.os.controller;
+
+import com.adison.os.domain.Tecnico;
+import com.adison.os.service.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tecnicos")
+public class TecnicoController {
+    @Autowired
+    private TecnicoService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+       Tecnico objectController = service.findById(id);
+       return ResponseEntity.ok().body(objectController);
+    }
 }
