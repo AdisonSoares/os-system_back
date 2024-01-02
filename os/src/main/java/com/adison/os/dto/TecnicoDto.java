10cd258af0e4d464384d233b27ce4b088def5c80
adison <adison.soares7@gmail.com>
2024-01-02 14:37:43 -0300

    :sparkles: Feat: add the Dto class for tecnico

--- os/src/main/java/com/adison/os/dto/TecnicoDto.java
+++ os/src/main/java/com/adison/os/dto/TecnicoDto.java
@@ -1,2 +1,28 @@
-package com.adison.os.dto;public class TecnicoDto {
+package com.adison.os.dto;
+
+import com.adison.os.domain.Tecnico;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.io.Serializable;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class TecnicoDto implements Serializable {
+    private Integer id;
+
+    private String nome;
+
+    @CPF
+    private String cpf;
+
+    private String telefone;
+
+    public TecnicoDto(Tecnico objectTecnico) {
+        this.id = objectTecnico.getId();
+        this.nome = objectTecnico.getNome();
+        this.cpf = objectTecnico.getCpf();
+        this.telefone = objectTecnico.getTelefone();
+    }
 }
