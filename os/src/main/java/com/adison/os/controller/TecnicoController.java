2648413a02a7a56df3df8293587316904e28cdef
adison <adison.soares7@gmail.com>
2024-01-03 11:21:29 -0300

    :sparkles: Feat: add the get all endpoint

--- os/src/main/java/com/adison/os/controller/TecnicoController.java
+++ os/src/main/java/com/adison/os/controller/TecnicoController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/tecnicos")
 public class TecnicoController {
@@ -21,5 +24,26 @@ public class TecnicoController {
        Tecnico objectController = service.findById(id);
        TecnicoDto objectDto = new TecnicoDto(objectController);
        return ResponseEntity.ok().body(objectDto);
+    }
+    @GetMapping
+    public ResponseEntity<List<TecnicoDto>> findAll(){
+        List<Tecnico> listTecnico = service.findAll();
+        List<TecnicoDto> listTecnicoDto= new ArrayList<>();
+
+        for (Tecnico objectTecnico : listTecnico) {
+            listTecnicoDto.add(new TecnicoDto(objectTecnico));
+        }
+
+        //ForEach simplificado
+        //listTecnico.forEach(objectTecnico -> listTecnicoDto.add(new TecnicoDto(objectTecnico)));
+
+        //Simplificação de tudo acima
+        /**
+         * List<TecnicoDto> listDto = service.findAll()
+         *                .stream().map(objectTecnico -> new TecnicoDto(objectTecnico))
+         *                .collect(Collectors.toList());
+         */
+
+        return ResponseEntity.ok().body(listTecnicoDto);
     }
 }
