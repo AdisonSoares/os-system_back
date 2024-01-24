763fe0df3a753e1b19dd680c4bb327ccea809f5e
adison <adison.soares7@gmail.com>
2024-01-24 18:48:20 -0300

    :sparkles: Feat: adds the OS findById endpoint

---
+++ os/src/main/java/com/adison/os/service/OrdenDeServicoService.java
@@ -0,0 +1,21 @@
+package com.adison.os.service;
+
+import com.adison.os.domain.OrdemDeServico;
+import com.adison.os.repositorie.OrdemDeServicoRepository;
+import com.adison.os.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class OrdenDeServicoService {
+    @Autowired
+    private OrdemDeServicoRepository ordemDeServicoRepository;
+
+    public OrdemDeServico findById(Integer id){
+        Optional<OrdemDeServico> objectOrdemDeServico = ordemDeServicoRepository.findById(id);
+        return objectOrdemDeServico.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: "+id+
+                ", Tipo: "+ OrdemDeServico.class.getName()));
+    }
+}
